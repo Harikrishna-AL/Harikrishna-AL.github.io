3a7760bcf45aadb24a1a1385ecc51f3d0db2ad1d
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2024-09-14 01:09:25 +0530

    update: projects and about

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ const fadeInOnScroll = () => {
     });
 };
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 window.addEventListener('scroll', fadeInOnScroll);
 
 // Trigger initial fade-in for elements already in view
